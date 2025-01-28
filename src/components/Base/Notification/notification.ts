62ec5866568891c705e0eeb35a5e7fabf66217bd
John <johnowusuwan@gmail.com>
2025-01-28 18:47:20 +0000

    notification error

--- src/components/Base/Notification/notification.ts
+++ src/components/Base/Notification/notification.ts
@@ -1,44 +1,44 @@
-import Toastify from "toastify-js";
-import { NotificationElement, NotificationProps } from "./index";
+// import Toastify from "toastify-js";
+// import { NotificationElement, NotificationProps } from "./index";
 
-const init = (el: NotificationElement, props: NotificationProps) => {
-  el.toastify = Toastify({
-    node: el,
-    duration: 3000,
-    newWindow: true,
-    close: false,
-    gravity: "top",
-    position: "right",
-    stopOnFocus: true,
-    ...props.options,
-    className: `${props.options.className || ""}`,
-    style: {
-      background: "none",
-      padding: "0",
-      boxShadow: "none",
-      ...props.options.style,
-    },
-  });
+// const init = (el: NotificationElement, props: NotificationProps) => {
+//   el.toastify = Toastify({
+//     node: el,
+//     duration: 3000,
+//     newWindow: true,
+//     close: false,
+//     gravity: "top",
+//     position: "right",
+//     stopOnFocus: true,
+//     ...props.options,
+//     className: `${props.options.className || ""}`,
+//     style: {
+//       background: "none",
+//       padding: "0",
+//       boxShadow: "none",
+//       ...props.options.style,
+//     },
+//   });
 
-  el.showToast = () => {
-    el.toastify.showToast();
-  };
+//   el.showToast = () => {
+//     el.toastify.showToast();
+//   };
 
-  el.hideToast = () => {
-    if (el.toastify) {
-      el.toastify.hideToast();
-    }
-  };
-};
+//   el.hideToast = () => {
+//     if (el.toastify) {
+//       el.toastify.hideToast();
+//     }
+//   };
+// };
 
-const reInit = (el: NotificationElement) => {
-  if (el && el.toastify) {
-    try {
-      el.hideToast();
-    } catch (error) {
-      console.debug('Toast already hidden or not initialized');
-    }
-  }
-};
+// const reInit = (el: NotificationElement) => {
+//   if (el && el.toastify) {
+//     try {
+//       el.hideToast();
+//     } catch (error) {
+//       console.debug('Toast already hidden or not initialized');
+//     }
+//   }
+// };
 
-export { init, reInit };
+// export { init, reInit };
