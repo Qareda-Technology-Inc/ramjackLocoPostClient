21c5ebdab3893d0014f4cedd4c6813634df4cda5
John <johnowusuwan@gmail.com>
2025-02-20 01:36:10 +0000

    axios change

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.100:2025/api',
-  // baseURL: "https://rjlserver.onrender.com/api",
+  // baseURL: 'http://192.168.0.100:2025/api',
+  baseURL: "https://rjlserver.onrender.com/api",
   headers: {
     'Content-Type': 'application/json'
   }
